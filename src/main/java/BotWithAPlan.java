66cb806652d49f5072ebede33ef2417b3920ff7e
yyyam0693 <e1354172@u.nus.edu>
2026-01-21 23:42:38 +0800

    Level-3: Mark and unmark tasks

--- src/main/java/BotWithAPlan.java
+++ src/main/java/BotWithAPlan.java
@@ -9,7 +9,7 @@ public class BotWithAPlan {
 
         Scanner sc = new Scanner(System.in);
 
-        String[] tasks = new String[MAX_TASKS];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
         printGreeting();
 
@@ -25,6 +25,7 @@ public class BotWithAPlan {
 
             if (input.equals("list")) {
                 printLine();
+                System.out.println(" I have a plan. Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println(" " + (i + 1) + ". " + tasks[i]);
                 }
@@ -32,13 +33,47 @@ public class BotWithAPlan {
                 continue;
             }
 
+            if (input.startsWith("mark ")) {
+                int index = parseIndex(input, "mark ");
+                if (index >= 1 && index <= taskCount) {
+                    Task t = tasks[index - 1];
+                    t.markAsDone();
+                    printLine();
+                    System.out.println(" OK, the plan is to mark this task as done, and ive gone ahead with the plan:");
+                    System.out.println(" " + t);
+                    printLine();
+                }
+                continue;
+            }
+
+            if (input.startsWith("unmark ")) {
+                int index = parseIndex(input, "unmark ");
+                if (index >= 1 && index <= taskCount) {
+                    Task t = tasks[index - 1];
+                    t.markAsNotDone();
+                    printLine();
+                    System.out.println(" OK, the plan is to mark this task as not done, and ive gone ahead with the plan:");
+                    System.out.println(" " + t);
+                    printLine();
+                }
+                continue;
+            }
+
             // add task
-            tasks[taskCount] = input;
+            tasks[taskCount] = new Task(input);
             taskCount++;
 
             printLine();
             System.out.println("added: " + input);
             printLine();
+        }
+    }
+
+    private static int parseIndex(String input, String prefix) {
+        try {
+            return Integer.parseInt(input.substring(prefix.length()).trim());
+        } catch (NumberFormatException e) {
+            return -1;
         }
     }
 
