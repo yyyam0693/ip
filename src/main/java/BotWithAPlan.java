310b8850604d497639a80c7f7834394d6c167fae
yyyam0693 <e1354172@u.nus.edu>
2026-01-21 23:17:16 +0800

    Level-2: Add and list tasks

--- src/main/java/BotWithAPlan.java
+++ src/main/java/BotWithAPlan.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 
 public class BotWithAPlan {
-    public static void main(String[] args) {
-        String logo = " ____        _   \n"
-                + "| __ )  ___ | |_    \n"
-                + "|  _ \\ / _ \\| __|  \n"
-                + "| |_) | (_) | |_    \n"
-                + "|____/ \\___/ \\__|  \n";
 
-        printLine();
-        System.out.println(" Hello! I'm Bot-With-A-Plan, a bot with a plan.");
-        System.out.println(" I have a plan... (that isn't planned yet). What can I do for you?");
-        System.out.println(logo);
-        printLine();
+    private static final int MAX_TASKS = 100;
+    private static final String LINE = "____________________________________________";
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+        printGreeting();
 
         while (true) {
             String input = sc.nextLine();
@@ -26,14 +23,41 @@ public class BotWithAPlan {
                 break;
             }
 
+            if (input.equals("list")) {
+                printLine();
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                printLine();
+                continue;
+            }
+
+            // add task
+            tasks[taskCount] = input;
+            taskCount++;
+
             printLine();
-            System.out.println(" " + input);
+            System.out.println("added: " + input);
             printLine();
         }
     }
 
+    private static void printGreeting() {
+        String logo = " ____        _   \n"
+                + "| __ )  ___ | |_    \n"
+                + "|  _ \\ / _ \\| __|  \n"
+                + "| |_) | (_) | |_    \n"
+                + "|____/ \\___/ \\__|  \n";
+
+        printLine();
+        System.out.println(" Hello! I'm Bot-With-A-Plan, a bot with a plan.");
+        System.out.println(" I have a plan... (that isn't planned yet). What can I do for you?");
+        System.out.println(logo);
+        printLine();
+    }
+
     private static void printLine() {
-        System.out.println("____________________________________________");
+        System.out.println(LINE);
     }
 
 }
