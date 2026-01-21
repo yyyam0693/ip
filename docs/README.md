37d921109cde72ed938125694d16fc020767d12b
yyyam0693 <e1354172@u.nus.edu>
2026-01-21 22:44:41 +0800

    Level-0: rename chatbot and add greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# BotWithAPlan User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/BotWithAPlan.java
@@ -0,0 +1,16 @@
+public class BotWithAPlan {
+    public static void main(String[] args) {
+        String logo = " ____        _   \n"
+                + "| __ )  ___ | |_    \n"
+                + "|  _ \\ / _ \\| __|  \n"
+                + "| |_) | (_) | |_    \n"
+                + "|____/ \\___/ \\__|  \n";
+
+        System.out.println("Hello! I'm Bot-With-A-Plan, a bot with a plan.");
+        System.out.println("I have a plan... (that isn't planned yet). What can I do for you?");
+        System.out.println(logo);
+        System.out.println("Bye. Hope to see you again soon!");
+
+
+    }
+}
